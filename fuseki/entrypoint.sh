ef961300439ba636e503530f0ba684e8784e7d05
LeonardDune <rl.wouters@gmail.com>
2026-03-09 20:44:47 +0100

    fix: pre-install curl/git in Fuseki image and use localhost URL (#285)

--- fuseki/entrypoint.sh
+++ fuseki/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# In de gecombineerde container draait Fuseki lokaal
+export FUSEKI_URL="${FUSEKI_URL:-http://localhost:3030}"
+
 # Start Fuseki via de originele entrypoint in de achtergrond
 /docker-entrypoint.sh &
 FUSEKI_PID=$!
